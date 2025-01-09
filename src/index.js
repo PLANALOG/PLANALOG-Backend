732c171e54f8a16271749f45efb963a46b1c86b5
YoungSeokLee <ylee102@gmail.com>
2025-01-10 00:48:53 +0900

    FIX: 프리즈마 스키마 수정 - 이름 규칙 - bigint 작업 및 id 타입 - varchar, text 등 상세타입 지정 - user의 platform, type 도 enum 으로 변경 - planner의 planner_date 타입 datetime - sharedplanner와 shared_planner_view - Post 의 cheering_count 를 liking_count - post와 momet/story 관계 일대일로 수정 - moment_content/story_content 의 sort_order int로 변경경

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@
 import dotenv from "dotenv";
 import express from 'express';          // -> ES Module
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient(); 
+// export const prisma = new PrismaClient({ log: ["query"] }); - sql 쿼리 확인 가능 옵션 (필요시 사용) 
 
 const app = express()
 const port = process.env.PORT;
@@ -11,9 +15,15 @@ app.use(express.static('public'));          // 정적 파일 접근
 app.use(express.json());                    // request의 본문을 json으로 해석할 수 있도록 함 (JSON 형태의 요청 body를 파싱하기 위함)
 app.use(express.urlencoded({ extended: false })); // 단순 객체 문자열 형태로 본문 데이터 해석
 
+BigInt.prototype.toJSON = function () { // bigint 호환
+  const int = Number.parseInt(this.toString());
+  return int ?? this.toString();
+};
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
