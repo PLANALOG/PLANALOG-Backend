49ebff4d08cf9b9483b281b5ea9f217cebcc6902
OhDongI <odi0716@naver.com>
2025-01-26 16:01:45 +0900

    Chore:충돌 해결

--- src/errors.js
+++ src/errors.js
@@ -53,6 +53,50 @@ export class PostIdNotFoundError extends Error{
   }
 }
 
+//중복 좋아요
+export class DuplicateLikePostError extends Error{
+  errorCode = "L001";
+
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.data = data;
+  }
+ }
+
+ //좋아요 존재 X
+ export class LikeIdNotExistError extends Error{ 
+  errorCode = "L002";
+
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.data = data;
+  }
+ }
+
+ //본인이 누른 좋아요 X
+ export class LikeNotOwnedByUserError extends Error{
+  errorCode = "L003";
+
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.data = data;
+ }
+}
+
+//postId X
+export class PostIdNotFoundError extends Error{
+  errorCode = "C001";
+
+  constructor(reason, data) {
+    super(reason);
+    this.reason = reason;
+    this.data = data;
+  }
+}
+
 
 export class LikeIdMissingError extends Error{
   errorCode = "L004";
